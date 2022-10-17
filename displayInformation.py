cf7102da11dcaf2f41b51300f783666cf6f46d1c
Trungvh2710 <113542803+Trungvh2710@users.noreply.github.com>
2022-10-17 09:13:15 +0700

    Add files via upload

---
+++ displayInformation.py
@@ -0,0 +1,5 @@
+print("Họ và tên: Vũ Hoàng Trung")
+print("Ngày sinh: 27/10/2000")
+print("Địa chỉ: Hà Nội")
+print("Nơi học tập: Học viện Ngân hàng")
+print("Nơi làm việc: Ngân hàng TMCP Đông Nam Á")
