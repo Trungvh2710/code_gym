f82b84c71c7e195c55c7b1d8cf543e74911d6500
Trungvh2710 <113542803+Trungvh2710@users.noreply.github.com>
2022-10-17 10:11:46 +0700

    Add files via upload

---
+++ Vẽ hình tròn.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.pensize (5)
+turtle.pencolor ("red")
+turtle.Turtle().goto(-40, 120)
+turtle.fillcolor ("red")
+turtle.begin_fill()
+turtle.circle (150)
+turtle.end_fill()
+
+turtle.done()
